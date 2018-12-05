056dbd0e4dd1639c3859d28de2d72ba90b866297
Buanaroti <belickyianton@mail.ru>
2018-12-05 23:16:52 +0300

    MMM2

--- src/js/main.js
+++ src/js/main.js
@@ -1,62 +1,41 @@
 // Нужно что бы за день происходили события:
-
+//
 // - Добавление новых клиентов и внесение средств
 // - Выход действующих клиентов и выплата всех средств
 // - Начисление процентов за день каждому клиенту из бюджета компании
 
 
-// // Баланс фирмы
-// let balance = 0;
-// // Процентная ставка в день
-// let percentDay = 0.01;
-//
-//
-// class Person{
-//     constructor(inviteMoney){
-//         this.bonus = 0;
-//         this.balance = 0;
-//
-//     }
-//
-//     inviteMoney(balance){
-//         this.balance = balance;
-//     }
-//
-//     getMoney(){
-//         this.balance = 0
-//     }
-// }
-//
-//
-//
-//
-// setInterval(function () {
-//     counter++;
-//     // let nameVar = 'people' + counter;
-//     var nameVar = new Person();
-//     // sasha.inviteMoney(Math.floor(Math.random() * 100000));
-//     console.log(counter);
-//
-// }, 5000);
+// Баланс фирмы
+let balance = 0;
+// Процентная ставка в день
+let percentDay = 0.01;
 
 
-function makeCounter() {
-    var currentCount = 1;
+class Person{
+    constructor(name){
+        this.bonus = 0;
+        this.balance = 0;
+        this.name = name;
 
-    return function() { // (**)
-        return currentCount++;
-    };
+    }
+
+    inviteMoney(balance){
+        this.balance = balance;
+        balance += this.balance;
+    }
+
+    getMoney(){
+        this.balance = 0
+    }
 }
 
-var counter = makeCounter(); // (*)
 
-// каждый вызов увеличивает счётчик и возвращает результат
-alert( counter() ); // 1
-alert( counter() ); // 2
-alert( counter() ); // 3
 
-// создать другой счётчик, он будет независим от первого
-var counter2 = makeCounter();
-alert( counter2() ); // 1
+setInterval(function () {
+    let vasa = new Person('vasa');
+    vasa.inviteMoney(Math.floor(Math.random() * 100000));
+    balance += vasa.balance;
+    console.log('Новый клиент ' + vasa.name);
+    console.log('Баланс фирмы ' + balance + ' р.');
 
-console.dir(counter);
+}, 5000);
